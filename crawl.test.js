c8d4d9c069e07799ccaa6c614357ea7622fd5722
Max-Frisch <max-frisch@gmx.net>
2023-12-18 17:34:28 +0700

    adding another test in jest

--- crawl.test.js
+++ crawl.test.js
@@ -14,3 +14,17 @@ test('normalizeURL strip slash at the end', () => {
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
 })
+
+test('normalizeURL capitals', () => {
+    const input = 'https://BLOG.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL http<s>', () => {
+    const input = 'http://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
